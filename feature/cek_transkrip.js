c97f4ca4686a33995d752cafee60e47a2d483960
ImLosss <pahriansyahryan@gmail.com>
2024-07-12 21:35:42 +0800

    update eden and database

--- feature/cek_transkrip.js
+++ feature/cek_transkrip.js
@@ -3,14 +3,18 @@ const { MessageMedia } = require('whatsapp-web.js');
 const fs = require('fs');
 
 const cekTranskrip = async (msg, client, sender) => {
-    let browser;
+    let browser, dialogHandler;
     const chat = await msg.getChat();
     try {
         let url = msg.body;
         url = url.split(' ');
         if(url.length <= 1) return msg.reply('Format anda salah, kirim kembali dengan format /cektranskrip [stb/nim]').catch(() => { chat.sendMessage('Format anda salah, kirim kembali dengan format /cektranskrip [stb/nim]') });
         url = url[1];
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox'],
+            executablePath: '/usr/bin/chromium-browser'
+         });
 
         // Membuat URL baru dengan parameter pencarian
         const newUrl = `https://siaka.undipa.ac.id/print.transkrip.php?stb=${encodeURIComponent(url)}`;
@@ -19,12 +23,22 @@ const cekTranskrip = async (msg, client, sender) => {
 
         const page = await browser.newPage();
 
+        // Mencegah dialog cetak
+        dialogHandler = async (dialog) => {
+            console.log(`Dialog message: ${dialog.message()}`);
+            await dialog.dismiss();
+        };
+
+        // Mencegah dialog cetak
+        page.on('dialog', dialogHandler);
+
         // Set header User-Agent
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36');
 
-        page.setDefaultTimeout(30000);
+        await page.setDefaultTimeout(15000);
+
         
-        page.goto(newUrl);
+        await page.goto(newUrl);
 
         await page.waitForSelector('body > div > table:nth-child(3) > tbody > tr:nth-child(2) > td > table.table-common > tbody > tr:nth-child(3)')
 
@@ -37,9 +51,16 @@ const cekTranskrip = async (msg, client, sender) => {
         .then(() => {
             fs.unlinkSync('./database/transkrip.png');
         })
+
+        // Hapus listener dialog setelah tidak diperlukan lagi
+        page.off('dialog', dialogHandler);
+
+        return await browser.close();
     } catch (err) {
         console.log(err)
-        msg.reply('Tidak menemukan data atau siaka sedang down').catch(() => { chat.sendMessage('Terjadi kesalahan atau siaka sedang down') })
+        msg.reply('Tidak menemukan data atau siaka sedang down').catch(() => { chat.sendMessage('Terjadi kesalahan atau siaka sedang down') });
+        page.off('dialog', dialogHandler);
+        return await browser.close();
     }
 }
 
