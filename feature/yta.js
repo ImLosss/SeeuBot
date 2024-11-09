2f3e8d70efafe014b3917494066f9c39285b286a
ImLosss <pahriansyahryan@gmail.com>
2024-11-09 21:03:38 +0800

    update yta

--- feature/yta.js
+++ feature/yta.js
@@ -286,7 +286,7 @@ const yta = async function (msg, sender, client) {
 
         if(duration <= min) {
             msg.reply('[⏳] Mulai mengunduh...')
-            const video = yt(myurl, { quality: 'highestaudio', format:'mp3', agent: agent });
+            const video = yt(myurl, { quality: '18', format:'mp4', agent: agent });
             video.pipe(fs.createWriteStream(path));
             video.on('error', (err) => {
                 console.log('Error:',err);
